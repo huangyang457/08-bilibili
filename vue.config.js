8047fe04b5ae8134c1feb866713e213b0572cfc9
huangyang457 <105434445+huangyang457@users.noreply.github.com>
2022-07-18 04:05:07 +0800

    03

--- vue.config.js
+++ vue.config.js
@@ -1,25 +1,4 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: [
-    'quasar'
-  ],
-
-  // configureWebpack: {
-  //   plugins: [
-  //     AutoImport({
-  //       resolvers: [ElementPlusResolver()],
-  //     }),
-  //     Components({
-  //       resolvers: [ElementPlusResolver()],
-  //     }),
-  //   ],
-  // }
   publicPath: './',
-
-  pluginOptions: {
-    quasar: {
-      importStrategy: 'kebab',
-      rtlSupport: false
-    }
-  }
 })
